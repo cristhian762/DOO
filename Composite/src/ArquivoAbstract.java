97bc2fb6ee294ce7a68eab94c6c74dc0400688ca
Cristhian Fontana Mattiuzzi <76015188+cristhian762@users.noreply.github.com>
2022-05-25 09:46:14 -0300

    Composite

---
+++ Composite/src/ArquivoAbstract.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 20191BSI0077
+ */
+abstract class ArquivoAbstract {
+    public String nome;
+    
+    public void printNomeDoArquivo(){
+        System.out.println(nome);
+    }
+}
---
+++ Composite/src/ArquivoComposite.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 20191BSI0077
+ */
+public class ArquivoComposite extends ArquivoAbstract {
+    public ArrayList listaDeArquivos(){
+        ArrayList list = new ArrayList();
+        
+        return list;
+    }
+    
+    
+}
